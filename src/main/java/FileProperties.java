02de2556ef15d3b41d2c802322607f33ef332678
Freeman2021 <Freeman3466@gmail.com>
2022-06-19 17:34:43 +0300

    init

---
+++ src/main/java/FileProperties.java
@@ -0,0 +1,13 @@
+
+public class FileProperties {
+
+    public static final String FILE_FIELD_DELIMITER = "\n";
+
+    public static final String INPUT_FILE_DIR = "./src/main/resources/files/input";
+
+    public static final String OUTPUT_FILE_DIR = "./src/main/resources/files/output/";
+
+    public static final String OUTPUT_FILE_NAME = "output.txt";
+
+    public static final String OUTPUT_FILE_DIR_AND_NAME = OUTPUT_FILE_DIR + OUTPUT_FILE_NAME;
+}
